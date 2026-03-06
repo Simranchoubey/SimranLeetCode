d1d8d2d789bc8817bc1854bbe4cfa1b8164327af
Simran. <simrankchoubey@gmail.com>
2026-03-06 18:54:11 +0530

    Time: 83 ms (49.89%) | Memory: 89.1 MB (23.91%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -9,16 +9,22 @@ public:
             st.insert(nums[i]);
         }
         for (auto it : st) {
-            if (st.find(it - 1) == st.end()) {
-                int cnt = 1; 
-                int x = it; 
-                while (st.find(x + 1) != st.end()) {
-                    x = x + 1; 
-                    cnt = cnt + 1; 
+            if (st.find(it - 1) == st.end()) { // 1-1=0 not in set so enter if loop
+// 1 exists in set, so this is not a sequence start, and the block is skipped.
+                int cnt = 1; //1
+                int x = it; //1 //100
+                while (st.find(x + 1) != st.end()) { //3  4. 5
+                    x = x + 1; //2 , 3, 4 no
+                    cnt = cnt + 1; //2  , 3 , 4
                 }
-                longest = max(longest, cnt);
+                longest = max(longest, cnt);// 1,4=4 // 4,1=4 
             }
         }
         return longest;
     }
 };
+//          0  1. 2. 3.4 5
+// nums = [100,4,200,1,3,2]  longest=1
+// st = {1,2,3,4,100,200}
+//                   it
+
