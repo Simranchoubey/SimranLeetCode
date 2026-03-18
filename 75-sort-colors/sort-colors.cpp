990f4b14812c3bfeb168eb4031255c5c26a8395d
Simran. <simrankchoubey@gmail.com>
2026-03-18 21:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.11%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,56 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0 ; //0 
-        int j = 0; //1
-        int k = n-1; //2
-        while(j <= k){
-            if(nums[j] == 1) j++;
-            else if(nums[j] == 2){
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int k=n-1;
+        while(j<=k){
+            if(nums[j]==1)j++;
+            else if(nums[j]==2){
                 swap(nums[j],nums[k]);
                 k--;
-            }else{
+            }
+            else{
                 swap(nums[j],nums[i]);
-                i++;
-                j++;
+                i++,j++;
             }
         }
     }
 };
-//         0 1 2 3 4 5
-// nums = [2,0,2,1,1,0]
-// i=0,j=0,k=5
-// 0<=5. 
-//   (nums[0]=2)==1  no
-//            2==2
-//             swap(nums[0],nums[5]){2,0} k--//4
-            
-//         0 1 2 3 4 5
-// nums = [0,0,2,1,1,2]            
-// i=0,j=0,k=4
-// nums[k]=1
-// num[j]=0==1 no
-//        0==2 no
-//        swap(0,0)
-//        i++,j++//1,1
-
-//         0 1 2 3 4 5
-// nums = [0,0,2,1,1,2]            
-// i=1,j=1,k=4
-// nums[k]=1
-// num[j]=0==1 no
-//        0==2 no
-//        swap(0,0)
-//        i++,j++//2,2     
-
-//         0 1 2 3 4 5
-// nums = [0,0,2,1,1,2]            
-// i=2,j=2,k=4
-// nums[k]=1
-// num[j]=2==1 no
-//        2==2 yes
-//        swap(nums[j],nums[k]) //{2,1}
-//        k--//3
-//         0 1 2 3 4 5
-// nums = [0,0,1,1,2,2]         ans 
