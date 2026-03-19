38a134463fb69f112b54a2ea80165b0129d22348
Simran. <simrankchoubey@gmail.com>
2026-03-19 21:11:16 +0530

    Time: 2 ms (30.82%) | Memory: 97.3 MB (86.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -10,26 +10,3 @@ public:
         return profit;
     }
 };
-
-// prices = [7,1,5,3,6,4]
-// maxProfit=0
-// bestBuy=prices[0]=7
-// i=0 
-// 7>7
-// bestBuy=min(bestBuy,prices[i])=min(7,7)=7
-// i=1
-// 1>7
-// bestBuy=min(bestBuy,prices[i])=min(7,1)=1
-// i=2
-// 5>7
-// bestBuy=min(bestBuy,prices[i])=min(7,5)=5
-// ans = 5
-// i=3
-// 3>7
-// bestBuy=min(bestBuy,prices[i])=min(7,3)=3
-// i=4
-// 6>7
-// bestBuy=min(bestBuy,prices[i])=min(7,6)=6
-// i=5
-// 4>7
-// bestBuy=min(bestBuy,prices[i])=min(7,4)=4
